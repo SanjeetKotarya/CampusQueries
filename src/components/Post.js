f7af1885f92d1478c96bd4a3f7c45b8c9675cbb6
SanjeetKotarya <sanjeetkotarya@gmail.com>
2023-09-27 22:18:04 +0530

    Your commit message here

--- src/components/Post.js
+++ src/components/Post.js
@@ -126,28 +126,7 @@ function Post({ id, question, image, time, quoraUser,searchQuery  }) {
       </div>
       <div className="post_footer">
         <div className="actions">
-          <span>
-            <a href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24"
-                viewBox="0 -960 960 960"
-                width="24"
-              >
-                <path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z" />
-              </svg>
-            </a>
-            <a href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24"
-                viewBox="0 -960 960 960"
-                width="24"
-              >
-                <path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z" />
-              </svg>
-            </a>
-          </span>
+
           <a
             className="show_answer"
             onClick={() => setShowAnswers(!showAnswers)}
@@ -162,8 +141,9 @@ function Post({ id, question, image, time, quoraUser,searchQuery  }) {
             </svg>
             <small>{answersCount}</small>
           </a>
+          <button onClick={() => setIsModalOpen(true)}>Add answer</button>
         </div>
-        <button onClick={() => setIsModalOpen(true)}>Add answer</button>
+        
         <div className="more">
           <a href="#">
             <svg
