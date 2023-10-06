1e4496c71fffd1158f821ec239da8654f1417e30
SanjeetKotarya <sanjeetkotarya@gmail.com>
2023-10-07 01:40:24 +0530

    Your commit message here

--- src/components/SidebarR.js
+++ src/components/SidebarR.js
@@ -1,15 +1,5 @@
 import "../css/SidebarR.css";
 import React, { useState, useRef, useEffect } from "react";
-import { Modal } from "react-responsive-modal";
-import "react-responsive-modal/styles.css";
-import { signOut } from "firebase/auth";
-import { auth, storage } from "../firebase";
-import db from "../firebase";
-import { useDispatch, useSelector } from "react-redux";
-import { logout, selectUser } from "../features/userSlice";
-import { collection, addDoc } from "firebase/firestore";
-import { serverTimestamp } from "firebase/firestore";
-import { ref, uploadString, getDownloadURL } from "firebase/storage";
 
 function SidebarR() {
   return (
